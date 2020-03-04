33708a8142a24f5c6e3dee3411e920818da17eba
kkmm <1054293440@qq.com>
2020-03-04 18:03:47 +0800

    新增weakifyVC

---
+++ Demo_2_RACweakify/WeakifyDemoVC.h
@@ -0,0 +1,17 @@
+//
+//  WeakifyDemoVC.h
+//  DemoForInterview
+//
+//  Created by kkmm on 2020/3/4.
+//  Copyright © 2020 kkmm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WeakifyDemoVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Demo_2_RACweakify/WeakifyDemoVC.m
@@ -0,0 +1,32 @@
+//
+//  WeakifyDemoVC.m
+//  DemoForInterview
+//
+//  Created by kkmm on 2020/3/4.
+//  Copyright © 2020 kkmm. All rights reserved.
+//
+
+#import "WeakifyDemoVC.h"
+
+@interface WeakifyDemoVC ()
+
+@end
+
+@implementation WeakifyDemoVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
